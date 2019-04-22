3272725064797082f755f777496a410416a0bcf6
Mario Andres <Marioarendon2@gmail.com>
2019-04-22 17:14:02 -0400

    change prod config

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
         'DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'store.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_TOKEN_LOCATION = ['headers', 'cookies']
-    JWT_COOKIE_SECURE = False  # change this to true in prod
+    JWT_COOKIE_SECURE = True  # change this to true in prod
     JWT_COOKIE_CRSF_PROTECT = True
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET')
     JWT_ACCESS_TOKEN_EXPIRES = 3600
